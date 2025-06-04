e97eda58e1ee7af0f8e53364f2a879c302d48f11
khanh <khanh.nguyen.contact@gmail.com>
2025-06-04 16:41:34 +0800

    auto_push

--- Main.lean
+++ Main.lean
@@ -1,4 +1,16 @@
-import Projecteuler
+import ProjectEuler.P1
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+def run (n: Nat): String :=
+  match n with
+    | 1 => P1.run
+    | _ => s!"no solution for problem {n}."
+
+
+def main (args : List String) : IO UInt32 := do
+  match args with
+  | x :: _ =>
+    match String.toNat? x with
+    | some n => IO.println (run n)
+    | none   => IO.println s!"could not parse '{x}' as a natural number."
+  | [] => IO.println "please provide a number as the first argument."
+  return 0
---
+++ ProjectEuler/P1.lean
@@ -0,0 +1,6 @@
+import ProjectEuler.Reduce
+
+namespace P1
+  def run : String :=
+    "hello world"
+end P1

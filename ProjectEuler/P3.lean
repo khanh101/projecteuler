20188455d5a59cf3a05cac872ef197ca83f71945
khanh <khanh.nguyen.contact@gmail.com>
2025-06-05 00:44:10 +0800

    auto_push

--- ProjectEuler/P3.lean
+++ ProjectEuler/P3.lean
@@ -4,9 +4,33 @@ namespace P3
 
   open Iterator
 
-  -- prime sieve
-  partial def prime: Iterator Nat Nat :=
-    let state := 0
+
+  -- prime test for c >= 3
+  partial def odd_prime_test (c: Nat): Bool :=
+    let rec loop (m: Nat): Bool :=
+      if m * m > c then true else
+      if c % m == 0 then false else
+        loop (m+2)
+
+    loop 3
+
+  partial def next_odd_prime (c: Nat): Nat :=
+    if odd_prime_test c then c else
+      next_odd_prime (c+2)
+
+
+  partial def prime :=
+    let prime_ge3: Iterator Nat Nat :=
+      let last_prime: Nat := 3
+      let next (last_prime: Nat): Option (Nat × Nat) :=
+        let next_prime := next_odd_prime (last_prime + 2)
+        some (next_prime, next_prime)
+
+      make_iterator next last_prime
+
+    prime_ge3.prepend [2, 3]
+
+  #eval let (_, x) := prime.take 10; x -- should be 10 primes
 
   partial def run (_: Unit): String :=
     let ps := prime

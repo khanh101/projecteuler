f78e0e556ef2e96ab9909df6cd7468ffa0de9291
khanh <khanh.nguyen.contact@gmail.com>
2025-06-05 00:44:51 +0800

    auto_push

--- ProjectEuler/P3.lean
+++ ProjectEuler/P3.lean
@@ -39,7 +39,7 @@ namespace P3
     let reduce (largest_factor: Nat) (p: Nat): Option Nat :=
       if p * p > n then none -- stop iteration here
         else
-        if (n % p) == 0 then some largest_factor -- previous largest factor
+        if n % p == 0 then some largest_factor -- previous largest factor
         else some p
 
     (ps.reduce reduce 0).repr

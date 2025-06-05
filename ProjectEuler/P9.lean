e421b62cc34daae7fdc1cd9ba1982610be5ef214
nguyen_ngoc_khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-06-05 16:56:19 +0800

    auto_push

--- ProjectEuler/P9.lean
+++ ProjectEuler/P9.lean
@@ -2,6 +2,15 @@ import ProjectEuler.Output
 
 namespace P9
 
+  -- m > n > 0
+  def pythagorean (m: Nat) (n: Nat): Nat × Nat × Nat :=
+    let a := m^2 - n^2
+    let b := 2 * m * n 
+    let c := m^2 + n^2
+    (a, b, c)
+  
+
+
   partial def run (_: Unit): Output :=
     sorry
 
